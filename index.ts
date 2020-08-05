fab622a1f6577fd3e18d5e67183076971337203c
violine1101 <violine1101@gmail.com>
2020-08-05 21:42:52 +0200

    Fix #83 (Mojira does not react to ticket numbers anymore) (#84)
    Also some more minor restructurings because why not

--- index.ts
+++ index.ts
@@ -13,6 +13,34 @@ log4js.configure( {
 
 try {
 	BotConfig.init();
+
+	const logConfig: log4js.Configuration = {
+		appenders: {
+			out: { type: 'stdout' },
+		},
+		categories: {
+			default: { appenders: [ 'out' ], level: BotConfig.debug ? 'debug' : 'info' },
+		},
+	};
+
+	if ( BotConfig.logDirectory ) {
+		logConfig.appenders.log = {
+			type: 'file',
+			filename: `${ BotConfig.logDirectory }/${ new Date().toJSON().replace( /[:.]/g, '_' ) }.log`,
+		};
+		logConfig.categories.default.appenders.push( 'log' );
+	}
+
+	log4js.configure( logConfig );
+
+	if ( BotConfig.debug ) {
+		MojiraBot.logger.info( 'Debug mode is activated' );
+	}
+
+	if ( BotConfig.logDirectory ) {
+		MojiraBot.logger.info( `Writing log to ${ logConfig.appenders.log[ 'filename' ] }` );
+	}
+
 	MojiraBot.start();
 } catch ( err ) {
 	MojiraBot.logger.error( err );
