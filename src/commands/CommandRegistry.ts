de5b1445133e1cb3fa6ad6ccd6434546071acf5e
chandler05 <66492208+chandler05@users.noreply.github.com>
2020-08-08 16:01:11 -0500

    ;)

--- src/commands/CommandRegistry.ts
+++ src/commands/CommandRegistry.ts
@@ -1,6 +1,7 @@
 import BugCommand from './BugCommand';
 import HelpCommand from './HelpCommand';
 import PingCommand from './PingCommand';
+import MooCommand from './MooCommand';
 import MentionCommand from './MentionCommand';
 import PollCommand from './PollCommand';
 import RolesCommand from './RolesCommand';
@@ -10,6 +11,7 @@ export default class CommandRegistry {
 	public static BUG_COMMAND = new BugCommand();
 	public static HELP_COMMAND = new HelpCommand();
 	public static MENTION_COMMAND = new MentionCommand();
+	public static MOO_COMMAND = new MooCommand();
 	public static PING_COMMAND = new PingCommand();
 	public static POLL_COMMAND = new PollCommand();
 	public static ROLES_COMMAND = new RolesCommand();
