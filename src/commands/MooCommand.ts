99ab9215e37ade636762d2b3270aa00e22f5254e
chandler05 <66492208+chandler05@users.noreply.github.com>
2020-08-08 18:03:38 -0500

    MooCommand Fixed

--- src/commands/MooCommand.ts
+++ src/commands/MooCommand.ts
@@ -1,9 +1,10 @@
-import { Message } from 'discord.js';
+import { Message, RichEmbed } from 'discord.js';
 import PrefixCommand from './PrefixCommand';
 import { SingleMention } from '../mentions/SingleMention';
+import { ReactionsUtil } from '../util/ReactionsUtil';
 
-export default class PingCommand extends PrefixCommand {
-	public readonly aliases = ['moo', 'test'];
+export default class MooCommand extends PrefixCommand {
+	public readonly aliases = ['moo', 'cow'];
 
 	public async run( message: Message, args: string ): Promise<boolean> {
 		if ( args.length ) {
@@ -11,19 +12,10 @@ export default class PingCommand extends PrefixCommand {
 		}
 
 		try {
-			new SingleMention("MC-772");
-		} catch {
-			return false;
-		}
-
-		try {
-			await message.react( '🐄' );
-		} catch {
-			return false;
-		}
-
-		try {
-			await message.react( '🥛' );
+			new SingleMention( 'MC-772' );
+			let embed: RichEmbed; 
+			await message.channel.send( embed );
+			await ReactionsUtil.reactToMessage( embed, ['🐄','🥛'] );
 		} catch {
 			return false;
 		}
