22d66c1d08bdaac08517b6f6fa63a336086ac797
chandler05 <66492208+chandler05@users.noreply.github.com>
2020-08-09 04:53:30 -0500

    Add a !jira moo command (#95)
    Co-authored-by: NeunEinser <daniel30797@gmail.com>
    Co-authored-by: violine1101 <violine1101@gmail.com>

---
+++ src/commands/MooCommand.ts
@@ -0,0 +1,29 @@
+import { Message } from 'discord.js';
+import PrefixCommand from './PrefixCommand';
+import { SingleMention } from '../mentions/SingleMention';
+import { ReactionsUtil } from '../util/ReactionsUtil';
+
+export default class MooCommand extends PrefixCommand {
+	public readonly aliases = ['moo', 'cow'];
+
+	public async run( message: Message, args: string ): Promise<boolean> {
+		if ( args.length ) {
+			return false;
+		}
+
+		try {
+			const mention = new SingleMention( 'MC-772' );
+			const embed = await mention.getEmbed();
+			await message.channel.send( embed );
+			await ReactionsUtil.reactToMessage( message, ['🐮', '🐄', '🥛'] );
+		} catch {
+			return false;
+		}
+
+		return true;
+	}
+
+	public asString(): string {
+		return '!jira moo';
+	}
+}
