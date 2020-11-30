be9e26a3f62d43546b90b0be110b3a531cf4081a
chandler05 <66492208+chandler05@users.noreply.github.com>
2020-11-30 11:08:01 -0600

    How did this happen 2.0?

--- src/commands/CommandRegistry.ts
+++ src/commands/CommandRegistry.ts
@@ -6,7 +6,6 @@ import MentionCommand from './MentionCommand';
 import PollCommand from './PollCommand';
 import RolesCommand from './RolesCommand';
 import ShutdownCommand from './ShutdownCommand';
-import TipsCommand from './TipsCommand';
 
 export default class CommandRegistry {
 	public static BUG_COMMAND = new BugCommand();
@@ -17,5 +16,4 @@ export default class CommandRegistry {
 	public static POLL_COMMAND = new PollCommand();
 	public static ROLES_COMMAND = new RolesCommand();
 	public static SHUTDOWN_COMMAND = new ShutdownCommand();
-	public static TIPS_COMMAND = new TipsCommand();
 }
