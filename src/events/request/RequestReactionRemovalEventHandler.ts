78ddd1338691a1a027a31cd843c0743149733b35
chandler05 <66492208+chandler05@users.noreply.github.com>
2020-11-30 21:47:02 -0600

    Update RequestReactionRemovalEventHandler.ts

--- src/events/request/RequestReactionRemovalEventHandler.ts
+++ src/events/request/RequestReactionRemovalEventHandler.ts
@@ -10,7 +10,7 @@ export default class RequestReactionRemovalEventHandler implements EventHandler<
 
 	// This syntax is used to ensure that `this` refers to the `RequestResolveEventHandler` object
 	public onEvent = async ( reaction: MessageReaction, user: User ): Promise<void> => {
-		this.logger.info( `User ${ user.tag } added '${ reaction.emoji.name }' reaction to request message '${ reaction.message.id } )
+		this.logger.info( `User ${ user.tag } added '${ reaction.emoji.name }' reaction to request message '${ reaction.message.id }'` );
 
 		reaction.users.remove(user);
 	};
