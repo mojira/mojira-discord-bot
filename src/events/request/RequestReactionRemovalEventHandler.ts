259718792fe6ba4f777bf2abec6a3575048d1869
chandler05 <aidentchandler@gmail.com>
2020-11-30 20:58:09 -0600

    Remove extra reactions from the request channels

---
+++ src/events/request/RequestReactionRemovalEventHandler.ts
@@ -0,0 +1,19 @@
+import { MessageReaction, User } from 'discord.js';
+import * as log4js from 'log4js';
+import TaskScheduler from '../../tasks/TaskScheduler';
+import EventHandler from '../EventHandler';
+
+export default class RequestReactionRemovalrEventHandler implements EventHandler<'messageReactionAdd'> {
+	public readonly eventName = 'messageReactionAdd';
+
+	private logger = log4js.getLogger( 'RequestResolutionRemovalEventHandler' );
+
+	// This syntax is used to ensure that `this` refers to the `RequestResolveEventHandler` object
+	public onEvent = async ( reaction: MessageReaction, user: User ): Promise<void> => {
+		this.logger.info( `User ${ user.tag } added '${ reaction.emoji.name }' reaction to request message '${ reaction.message.id }'` );
+
+		TaskScheduler.clearMessageTasks( reaction.message );
+
+		reaction.users.remove(user);
+	};
+}
