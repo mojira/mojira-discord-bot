62352da742f65392b3526688b3100fab8126176d
chandler05 <aidentchandler@gmail.com>
2020-12-03 09:15:28 -0600

    commit message issue

--- src/events/mention/MentionDeleteEventHandler.ts
+++ src/events/mention/MentionDeleteEventHandler.ts
@@ -14,7 +14,7 @@ export default class MentionDeleteEventHandler implements EventHandler<'messageR
 	const embeds = message.embeds;
         const mentionEmbed = embeds[0];
         const footer: string = mentionEmbed.footer.text;
-        const userTag = footer.match( /.+#\d{4}/ )[0];
+        const userTag = footer.match( /.{3,32}#[0-9]{4}/ )[0];
 
         if ( user.tag === userTag ) {
             try {
