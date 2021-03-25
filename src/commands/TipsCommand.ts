801c140a74535d0ce1edb7412b61e4ffe08d1087
chandler05 <66492208+chandler05@users.noreply.github.com>
2021-03-24 20:49:35 -0500

    Fix role selection channel mention in tips command

--- src/commands/TipsCommand.ts
+++ src/commands/TipsCommand.ts
@@ -12,7 +12,7 @@ export default class TipsCommand extends PrefixCommand {
 				For help with using the bug tracker, there is an article on the Minecraft website that you can read: <https://help.minecraft.net/hc/articles/360049840492>.
 				
 				How to use this server: 
-				Start by choosing which bug tracker projects you would like to be a part of in #role-selection.
+				Start by choosing which bug tracker projects you would like to be a part of in <#648479533246316555>.
 				Afterwards, you can use corresponding request channels in each project to make requests for changes to tickets on the bug tracker, like resolutions and adding affected versions. 
 				The moderators and helpers of the bug tracker will then be able to see the requests and resolve them.`.replace( /\t/g, '' )
 			);
