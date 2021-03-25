48deca43acdc5b467cab5f3ec66489e7246c813e
chandler05 <66492208+chandler05@users.noreply.github.com>
2021-03-24 21:11:07 -0500

    Tips command into embed

--- src/commands/TipsCommand.ts
+++ src/commands/TipsCommand.ts
@@ -6,16 +6,16 @@ export default class TipsCommand extends PrefixCommand {
 
 	public async run( message: Message ): Promise<boolean> {
 		try {
-			await message.channel.send(
-				`Welcome to the Mojira Discord Server! 
+			const embed = new MessageEmbed();
+			embed.setDescription( `__Welcome to the Mojira Discord Server!__
 				
 				For help with using the bug tracker, there is an article on the Minecraft website that you can read: <https://help.minecraft.net/hc/articles/360049840492>.
 				
 				How to use this server: 
 				Start by choosing which bug tracker projects you would like to be a part of in <#648479533246316555>.
 				Afterwards, you can use corresponding request channels in each project to make requests for changes to tickets on the bug tracker, like resolutions and adding affected versions. 
-				The moderators and helpers of the bug tracker will then be able to see the requests and resolve them.`.replace( /\t/g, '' )
-			);
+				The moderators and helpers of the bug tracker will then be able to see the requests and resolve them.`.replace( /\t/g, '' ) );
+			await message.channel.send( embed );
 		} catch {
 			return false;
 		}
