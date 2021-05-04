0e998b59409ce93beb7843d97a4a1f46692240a8
chandler05 <66492208+chandler05@users.noreply.github.com>
2021-05-04 10:35:29 -0500

    Switch help and tips commands to embeds (#180)
    * Format help command in an embed
    
    * Tips command into embed
    
    * Update TipsCommand.ts
    
    * Update HelpCommand.ts
    
    * Update HelpCommand.ts
    
    * Update TipsCommand.ts
    
    * Update HelpCommand.ts
    
    * Update TipsCommand.ts

--- src/commands/HelpCommand.ts
+++ src/commands/HelpCommand.ts
@@ -1,4 +1,4 @@
-import { Message } from 'discord.js';
+import { Message, MessageEmbed } from 'discord.js';
 import PrefixCommand from './PrefixCommand';
 import BotConfig from '../BotConfig';
 
@@ -7,19 +7,19 @@ export default class HelpCommand extends PrefixCommand {
 
 	public async run( message: Message ): Promise<boolean> {
 		try {
-			await message.channel.send(
-				`<:mojira:821162280905211964> MojiraBot help <:mojira:821162280905211964>
+			const embed = new MessageEmbed();
+			embed.setTitle( '<:mojira:821162280905211964> **MojiraBot help** <:mojira:821162280905211964>' )
+				.setDescription( `This is a bot that links to a Mojira ticket when its ticket number is mentioned.
+					Currently, the following projects are supported: ${ BotConfig.projects.join( ', ' ) }
+					To prevent the bot from linking a ticket, preface the ticket number with an exclamation mark.
 
-				This is a bot that links to a Mojira ticket when its ticket number is mentioned.
-				Currently, the following projects are supported: ${ BotConfig.projects.join( ', ' ) }
-				To prevent the bot from linking a ticket, preface the ticket number with an exclamation mark.
-				
-				To check whether the bot is currently running, you can use \`!jira ping\`. This bot is continuously being worked on and this will receive more features in the future.
-				It is not possible to invite this bot to other servers yet.
-				If you have any issues, feel free to ping violine1101.
-				
-				(For help with the bug tracker, use !jira tips)`.replace( /\t/g, '' )
-			);
+					To check whether the bot is currently running, you can use \`!jira ping\`. This bot is continuously being worked on and this will receive more features in the future.
+					It is not possible to invite this bot to other servers yet.
+					If you have any issues, feel free to ping violine1101.
+
+					(For help with the bug tracker, use \`!jira tips\`)`.replace( /\t/g, '' ) )
+				.setFooter( message.author.tag, message.author.avatarURL() );
+			await message.channel.send( embed );
 		} catch {
 			return false;
 		}
