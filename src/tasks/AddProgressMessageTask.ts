f0106e68d40e6cac694683a5ddf8609ce771c2f7
chandler05 <66492208+chandler05@users.noreply.github.com>
2021-05-05 13:25:10 -0500

    Update AddProgressMessageTask.ts

--- src/tasks/AddProgressMessageTask.ts
+++ src/tasks/AddProgressMessageTask.ts
@@ -1,23 +1,19 @@
-import { EmojiResolvable, Message, MessageEmbed, TextChannel, User } from 'discord.js';
-import BotConfig from '../BotConfig';
-import DiscordUtil from '../util/DiscordUtil';
-import { RequestsUtil } from '../util/RequestsUtil';
+import { Message } from 'discord.js';
 import MessageTask from './MessageTask';
 import * as log4js from 'log4js';
-import RequestDeleteEventHandler from '../events/request/RequestDeleteEventHandler';
 
 export default class AddProgressMessageTask extends MessageTask {
 	private static logger = log4js.getLogger( 'AddProgressMessageTask' );
 
 	private readonly request: Message;
 
-	constructor(  request: Message ) {
+	constructor( request: Message ) {
 		super();
 		this.request = request;
 	}
 
 	public async run( origin: Message ): Promise<void> {
-		// If the message has been deleted, don't do anything
+        // If the message has been deleted, don't do anything
 		if ( origin === undefined ) return;
 
         const comment = origin.content;
@@ -33,12 +29,12 @@ export default class AddProgressMessageTask extends MessageTask {
 
 		if ( comment ) {
             try {
-                let embed = this.request.embeds[0];
+                const embed = this.request.embeds[0];
                 embed.addField( date.toDateString(), comment );
                 await this.request.edit( embed );
             } catch ( error ) {
                 AddProgressMessageTask.logger.error( error );
-            }		
+            }
         }
 	}
 }
