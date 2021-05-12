da1724f51cfca8757acaf74bc22f341ac8406c0c
chandler05 <66492208+chandler05@users.noreply.github.com>
2021-05-12 16:00:15 -0500

    Add command for sending messages as MojiraBot (#147)
    Co-authored-by: violine1101 <violine1101@gmail.com>

--- src/commands/CommandRegistry.ts
+++ src/commands/CommandRegistry.ts
@@ -4,6 +4,7 @@ import PingCommand from './PingCommand';
 import MooCommand from './MooCommand';
 import MentionCommand from './MentionCommand';
 import PollCommand from './PollCommand';
+import SendCommand from './SendCommand';
 import SearchCommand from './SearchCommand';
 import ShutdownCommand from './ShutdownCommand';
 import TipsCommand from './TipsCommand';
@@ -15,6 +16,7 @@ export default class CommandRegistry {
 	public static MOO_COMMAND = new MooCommand();
 	public static PING_COMMAND = new PingCommand();
 	public static POLL_COMMAND = new PollCommand();
+	public static SEND_COMMAND = new SendCommand();
 	public static SEARCH_COMMAND = new SearchCommand();
 	public static SHUTDOWN_COMMAND = new ShutdownCommand();
 	public static TIPS_COMMAND = new TipsCommand();
