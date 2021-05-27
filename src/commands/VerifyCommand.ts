bd41ae547bf3af4ffc2f714f98b2bb47c33d2685
dericksonmark <markdericksonacct@gmail.com>
2021-05-26 21:04:43 -0400

    Fix some syntax issues

--- src/commands/VerifyCommand.ts
+++ src/commands/VerifyCommand.ts
@@ -1,4 +1,4 @@
-import { Message, MessageEmbed, TextChannel, User } from 'discord.js';
+import { Message, MessageEmbed, TextChannel } from 'discord.js';
 import PrefixCommand from './PrefixCommand';
 import BotConfig from '../BotConfig';
 import Command from './Command';
@@ -19,12 +19,12 @@ export default class VerifyCommand extends PrefixCommand {
 		if ( pendingChannel instanceof TextChannel ) {
 
 			let foundUser = false;
-			
-			const allMessages = await pendingChannel.messages.fetch({ limit: 100 })
+
+			const allMessages = await pendingChannel.messages.fetch( { limit: 100 } );
 
 			allMessages.forEach( async thisMessage => {
 				if ( thisMessage.embeds === undefined ) return undefined;
-				if ( thisMessage.embeds[0].fields[0].value.replace(/[<>@!]/g, '' ) === message.author.id ) {
+				if ( thisMessage.embeds[0].fields[0].value.replace( /[<>@!]/g, '' ) === message.author.id ) {
 					foundUser = true;
 				}
 			} );
@@ -32,19 +32,19 @@ export default class VerifyCommand extends PrefixCommand {
 			try {
 				const role = await pendingChannel.guild.roles.fetch( BotConfig.verifiedRole );
 				const targetUser = message.guild.members.fetch( message.author.id );
-				
-				if ( (await targetUser).roles.cache.has( role.id ) ) {
-					message.reply( 'Your account has already been verified!' )
-					message.react( '❌' )
+
+				if ( ( await targetUser ).roles.cache.has( role.id ) ) {
+					await message.channel.send( `${ message.author }, your account has already been verified!` );
+					await message.react( '❌' );
 					return false;
 				}
 			} catch ( error ) {
-				Command.logger.error( error )
+				Command.logger.error( error );
 			}
 
 			if ( foundUser ) {
-				message.reply( 'You already have a pending verification request!' )
-				message.react( '❌' )
+				await message.channel.send( `${ message.author }, you already have a pending verification request!` );
+				await message.react( '❌' )
 				return false;
 			}
 
@@ -52,10 +52,9 @@ export default class VerifyCommand extends PrefixCommand {
 
 		try {
 			const userEmbed = new MessageEmbed();
-			const token = this.randomString(15, '23456789abcdeghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ');
+			const token = this.randomString( 15, '23456789abcdeghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ' );
 
-			userEmbed.setDescription(`In order to verify, please comment the following token on the ticket [${ BotConfig.verificationTicket }](https://bugs.mojang.com/browse/${ BotConfig.verificationTicket }) using your Jira account. Make sure you only have added one comment to the ticket!\n\nToken: **${ token }**`
-			);
+			userEmbed.setDescription( `In order to verify, please comment the following token on the ticket [${ BotConfig.verificationTicket }](https://bugs.mojang.com/browse/${ BotConfig.verificationTicket }) using your Jira account. Make sure you only have added one comment to the ticket!\n\nToken: **${ token }**` );
 
 			message.author.send( userEmbed );
 
@@ -90,9 +89,9 @@ export default class VerifyCommand extends PrefixCommand {
 	}
 
 	// https://stackoverflow.com/questions/10726909/random-alpha-numeric-string-in-javascript
-	private randomString(length: number, chars: string) {
+	private randomString( length: number, chars: string ): string {
 		var result = '';
-		for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+		for ( var i = length; i > 0; --i ) result += chars[Math.floor( Math.random() * chars.length )];
 		return result;
 	}
 
