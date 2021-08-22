4db4a36e0f2c754717c3bbedb52c220126f22592
dericksonmark <68699877+dericksonmark@users.noreply.github.com>
2021-08-21 21:43:20 -0400

    Update src/util/LoggerUtil.ts
    Co-authored-by: SPGoding <15277496+SPGoding@users.noreply.github.com>

--- src/util/LoggerUtil.ts
+++ src/util/LoggerUtil.ts
@@ -2,8 +2,8 @@ export class LoggerUtil {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	public static shortenJiraError( error: any ): string {
 		return `Information:
-		  Status code: ${ error.response?.status || undefined }
-		  Status text: ${ error.response?.statusText || undefined }
-		  Error message(s): ${ error.response?.data.errorMessages || undefined }`.replace( /\t/g, '' );
+		  Status code: ${ error?.response?.status }
+		  Status text: ${ error?.response?.statusText }
+		  Error message(s): ${ error?.response?.data?.errorMessages }`.replace( /\t/g, '' );
 	}
 }
