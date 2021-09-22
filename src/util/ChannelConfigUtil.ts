83c32c076b879b20f0cad12be306ecfae2309ef2
chandler05 <66492208+chandler05@users.noreply.github.com>
2021-09-22 07:26:17 -0500

    Add thread channel support

--- src/util/ChannelConfigUtil.ts
+++ src/util/ChannelConfigUtil.ts
@@ -4,7 +4,7 @@ export class ChannelConfigUtil {
 	// Indicates in the channel's description that mentions are disabled in that channel.
 	// Tag: ~no-mention
 	public static mentionsDisabled( channel: TextBasedChannels ): boolean {
-		if ( channel instanceof TextChannel || channel instanceof NewsChannel ) {
+		if ( channel instanceof TextChannel || channel instanceof NewsChannel || channel instanceof ThreadChannel ) {
 			return channel.topic != null && channel.topic.includes( '~no-mention' );
 		}
 		return false;
@@ -13,7 +13,7 @@ export class ChannelConfigUtil {
 	// Indicates in the channel's description that commands are disabled in that channel.
 	// Tag: ~no-command
 	public static commandsDisabled( channel: TextBasedChannels ): boolean {
-		if ( channel instanceof TextChannel || channel instanceof NewsChannel ) {
+		if ( channel instanceof TextChannel || channel instanceof NewsChannel || channel instanceof ThreadChannel ) {
 			return channel.topic != null && channel.topic.includes( '~no-command' );
 		}
 		return false;
@@ -22,7 +22,7 @@ export class ChannelConfigUtil {
 	// Indicates in the channel's description that mention embeds will have limited information.
 	// Tag: ~limited-info
 	public static limitedInfo( channel: TextBasedChannels ): boolean {
-		if ( channel instanceof TextChannel || channel instanceof NewsChannel ) {
+		if ( channel instanceof TextChannel || channel instanceof NewsChannel || channel instanceof ThreadChannel ) {
 			return channel.topic != null && channel.topic.includes( '~limited-info' );
 		}
 		return false;
