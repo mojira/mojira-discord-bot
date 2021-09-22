3a023b0dfa7c724666b7b66738f8918d1602862d
chandler05 <66492208+chandler05@users.noreply.github.com>
2021-09-22 08:21:48 -0500

    Nevermind about thread channels

--- src/util/ChannelConfigUtil.ts
+++ src/util/ChannelConfigUtil.ts
@@ -1,10 +1,10 @@
-import { NewsChannel, TextChannel, ThreadChannel, TextBasedChannels } from 'discord.js';
+import { NewsChannel, TextChannel, TextBasedChannels } from 'discord.js';
 
 export class ChannelConfigUtil {
 	// Indicates in the channel's description that mentions are disabled in that channel.
 	// Tag: ~no-mention
 	public static mentionsDisabled( channel: TextBasedChannels ): boolean {
-		if ( channel instanceof TextChannel || channel instanceof NewsChannel || channel instanceof ThreadChannel ) {
+		if ( channel instanceof TextChannel || channel instanceof NewsChannel ) {
 			return channel.topic != null && channel.topic.includes( '~no-mention' );
 		}
 		return false;
@@ -13,7 +13,7 @@ export class ChannelConfigUtil {
 	// Indicates in the channel's description that commands are disabled in that channel.
 	// Tag: ~no-command
 	public static commandsDisabled( channel: TextBasedChannels ): boolean {
-		if ( channel instanceof TextChannel || channel instanceof NewsChannel || channel instanceof ThreadChannel ) {
+		if ( channel instanceof TextChannel || channel instanceof NewsChannel ) {
 			return channel.topic != null && channel.topic.includes( '~no-command' );
 		}
 		return false;
@@ -22,7 +22,7 @@ export class ChannelConfigUtil {
 	// Indicates in the channel's description that mention embeds will have limited information.
 	// Tag: ~limited-info
 	public static limitedInfo( channel: TextBasedChannels ): boolean {
-		if ( channel instanceof TextChannel || channel instanceof NewsChannel || channel instanceof ThreadChannel ) {
+		if ( channel instanceof TextChannel || channel instanceof NewsChannel ) {
 			return channel.topic != null && channel.topic.includes( '~limited-info' );
 		}
 		return false;
