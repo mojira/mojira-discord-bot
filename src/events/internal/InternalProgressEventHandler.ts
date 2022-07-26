c30ac50785100d5d9decb81467d477638b980436
dericksonmark <68699877+dericksonmark@users.noreply.github.com>
2022-07-26 16:13:34 -0400

    Update InternalProgressEventHandler.ts

--- src/events/internal/InternalProgressEventHandler.ts
+++ src/events/internal/InternalProgressEventHandler.ts
@@ -12,7 +12,7 @@ export default class InternalProgressEventHandler implements EventHandler<'messa
 	private logger = log4js.getLogger( 'InternalProgressEventHandler' );
 
 	private isValidId( id: string ): id is Snowflake {
-		return /^[0-9]{18,19}$/.test( id );
+		return /^[0-9]{18,}$/.test( id );
 	}
 
 	// This syntax is used to ensure that `this` refers to the `InternalProgressEventHandler` object
