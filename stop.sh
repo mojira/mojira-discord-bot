86feb903fba6056fe229baa6b676dab7fab14d9c
violine1101 <12451842+violine1101@users.noreply.github.com>
2026-06-30 20:19:07 +0200

    Update stop.sh script to respect deployment argument

--- stop.sh
+++ stop.sh
@@ -1,2 +1,4 @@
+# Make it so the default deployment is "main"
+DEPLOYMENT=${1:-main}
 # Send ^C to screen session window
-screen -S discordbot-$1 -X stuff $'\003'
+screen -S discordbot-$DEPLOYMENT -X stuff $'\003'
